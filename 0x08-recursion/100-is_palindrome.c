cb2b0c3926258993890743da740f1d59b5738de0
Philani <phs.sithole@gmail.com>
2023-07-05 05:01:48 -0700

    recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -4,19 +4,14 @@ int check_pal(char *s, int i, int len);
 int _strlen_recursion(char *s);
 
 /**
-* check_pal - checks the characters recursively for palindrome
-* @s: string to check
-* @i: iterator
-* @len: length of the string
+* _strlen_recursion - returns the length of a string
+* @s: string to calculate the length of
 *
-* Return: 1 if palindrome, 0 if not
+* Return: length of the string
 */
-int check_pal(char *s, int i, int len)
+int _strlen_recursion(char *s)
 {
-	if (*(s + i) != *(s + len - 1))
+	if (*s == '\0')
 		return (0);
-	if (i >= len)
-		return (1);
-	return (check_pal(s, i + 1, len - 1));
+	return (1 + _strlen_recursion(s + 1));
 }
-
